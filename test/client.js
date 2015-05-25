9777df550673c15ee38afcfd1d72c53bf2f57c40
Tim Emiola <temiola@google.com>
2015-05-24 19:54:16 -0700

    Add DecodedResponse < IncomingResponse

--- test/client.js
+++ test/client.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var decodeMessage = require('../lib/codec').decodeMessage;
+var encodeMessage = require('../lib/codec').encodeMessage;
 var expect = require('chai').expect;
 var fs = require('fs');
 var path = require('path');
+var nodeutil = require('util');
 var util = require('./util');
 
 var Stub = require('../lib/client').Stub;
@@ -37,22 +40,31 @@ http2.globalAgent = new http2.Agent({ log: util.clientLog });
 
 describe('client', function() {
   describe('test scenario', function() {
-    describe('request-response', function() {
+    describe('request_response', function() {
       it('should work as expected', function(done) {
         var path = '/x';
-        var message = 'Hello world';
-        var server = http2.createServer(options, function(request, response) {
-          expect(request.url).to.equal(path);
-          response.end(message);
-        });
         var request_text = 'hello';
         var response_text = 'world';
+        var server = http2.createServer(options, function(request, response) {
+          expect(request.url).to.equal(path);
+          var replyEncoded = function(encoded) {
+            response.end(encoded);
+          };
+          var checkEncRq = function(err, decoded){
+            expect(err).to.be.null;
+            expect(decoded.toString()).to.equal(request_text);
+            encodeMessage(response_text, null, replyEncoded);
+          };
+          request.once('data', function(data) {
+            decodeMessage(data, checkEncRq);
+          });
+        });
 
         server.listen(1505, function() {
-          var stub = new Stub('https://localhost:1505' + path);
+          var stub = new Stub('https://localhost:1505');
           stub.request_response(path, request_text, function(response) {
             response.on('data', function(data) {
-              expect(data.toString()).to.equal(message);
+              expect(data.toString()).to.equal(response_text);
             });
 
             response.on('end', function() {
