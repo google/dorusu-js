674fe7c03c2f23a4c6a6bc69926da0b1abf28feb
Tim Emiola <temiola@google.com>
2015-06-14 16:35:57 -0700

    Adds codes and code/string conversion funcs.

--- test/nurpc.js
+++ test/nurpc.js
@@ -13,6 +13,7 @@ var options = {
   log: util.serverLog
 };
 
+
 // typical tests should
 // - start a http2 server
 // - send a request using the grpc surface
@@ -33,25 +34,15 @@ var options = {
 
 http2.globalAgent = new http2.Agent({ log: util.clientLog });
 
-describe('nurpc module functions', function() {
-  describe('isReservedHeader', function() {
+describe('nurpc', function() {
+  describe('method `isReservedHeader(headerName)`', function() {
     var colonStarters = [':random', ':authority', ':host'];
-    colonStarters.forEach(function(h) {
+      colonStarters.forEach(function(h) {
       it('should be true for ' + h, function() {
         expect(nurpc.isReservedHeader(h)).to.be.true;
       });
     });
-    var reservedHeaders =  [
-      'content-type',
-      'grpc-encoding',
-      'grpc-message',
-      'grpc-message-type',
-      'grpc-status',
-      'grpc-timeout',
-      'te',
-      'user-agent'
-    ];
-    reservedHeaders.forEach(function(h) {
+    nurpc.reservedHeaders.forEach(function(h) {
       it('should be true for ' + h, function() {
         expect(nurpc.isReservedHeader(h)).to.be.true;
       });
@@ -64,6 +55,37 @@ describe('nurpc module functions', function() {
     unreservedHeaders.forEach(function(h) {
       it('should be false for ' + h, function() {
         expect(nurpc.isReservedHeader(h)).to.be.false;
+      });
+    });
+  });
+  describe('method `h2NameToRpcName`', function() {
+    it('should return UNKNOWN for an invalid name', function() {
+      expect(nurpc.h2NameToRpcName('foo')).to.eql('UNKNOWN');
+    });
+    var unmapped = ['HTTP_1_1_REQUIRED', 'STREAM_CLOSED'];
+    var h2Codes = nurpc.h2Codes;
+    h2Codes.forEach(function(c) {
+      if (unmapped.indexOf(c) == -1) {
+        it('should return a valid name for ' + c, function() {
+          expect(nurpc.h2NameToRpcName(c)).to.be.ok;
+          expect(nurpc.h2NameToRpcName(c)).to.not.eql('UNKNOWN');
+          });
+      }
+    });
+    unmapped.forEach(function(c) {
+      it('should return null for ' + c, function() {
+        expect(nurpc.h2NameToRpcName(c)).to.be.null;
+      });
+    });
+  });
+
+  describe('method `rpcCode`', function() {
+    it('should throw an exception for unknown names', function() {
+      expect(function() { nurpc.rpcCode('foo'); }).to.throw(Error);
+    });
+    nurpc.rpcCodes.forEach(function(c) {
+      it('should return a valid code for ' + c, function() {
+        expect(nurpc.rpcCode(c)).to.be.at.least(0);
       });
     });
   });
