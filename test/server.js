3629abbd6ab630099773e2f421188d2e68e5bbf3
Tim Emiola <temiola@google.com>
2015-06-20 13:23:32 -0700

    Adds support handling apps to the rpc server
    - apps specify a set of required handlers and the {un,}marshallers that
      they use
    - `app` is one of the options in the server constructor, when specified,
      the app will be served
    - if a requestListener is also specified in the server constructor, it
      acts a fallback for the app

--- test/server.js
+++ test/server.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var _ = require('lodash');
+var app = require('../lib/app');
 var expect = require('chai').expect;
 var http2 = require('http2');
+var irreverser = require('./util').irreverser;
 var insecureOptions = require('./util').insecureOptions;
 var listenOnFreePort = require('./util').listenOnFreePort;
+var reverser = require('./util').reverser;
 var nurpc = require('../lib/nurpc');
 var secureOptions = require('./util').secureOptions;
 var server = require('../lib/server');
@@ -12,8 +15,45 @@ var server = require('../lib/server');
 var Stub = require('../lib/client').Stub;
 
 
-// Tests here can rely on the nurpc client being available in tests as it is
-// tested without any dependencies on RpcServer.
+// testTable is used to verify nurpc.makeDispatcher.
+var testTable = {
+  '/x': function xHandler(request, response) {
+    request.once('data', function(data) {
+      response.end('response from /x');
+    });
+  },
+  '/y': function yHandler(request, response) {
+    request.once('data', function(data) {
+      response.end('response from /y');
+    });
+  }
+};
+
+// testApp is used to verify app handling
+var testApp = new app.RpcApp(
+  app.Service('test', [
+    app.Method('do_echo', reverser, irreverser),
+    app.Method('do_reverse', reverser),
+    app.Method('do_irreverse', null, reverser)
+  ])
+);
+testApp.register('/test/do_echo', function testHandler(request, response) {
+  request.once('data', function(data) {
+    response.end(data);
+  });
+});
+testApp.register('/test/do_reverse', function testHandler(request, response) {
+  request.once('data', function(data) {
+    response.end(data);
+  });
+});
+testApp.register('/test/do_irreverse', function testHandler(request, response) {
+  request.once('data', function(data) {
+    response.end(data);
+  });
+});
+
+// Tests here can use the nurpc client as it's tests do not depend on RpcServer.
 //
 // Typically flow is:
 // - start a RpcServer
@@ -22,19 +62,6 @@ var Stub = require('../lib/client').Stub;
 // - optionally verify what the client receives using the nurpc
 
 describe('RpcServer', function() {
-  var testTable = {
-    '/x': function yHandler(request, response) {
-      request.once('data', function(data) {
-        response.end('response from /x');
-      });
-    },
-    '/y': function yHandler(request, response) {
-      request.once('data', function(data) {
-        response.end('response from /y');
-      });
-    }
-  };
-
   var nonBinMd = {
     trailer1: 'value1',
     trailer2: 'value2'
@@ -60,7 +87,144 @@ describe('RpcServer', function() {
     insecure: insecureOptions
   };
   _.forEach(testOptions, function(serverOptions, connType) {
-    describe(connType + ': `server.makeDispatcher`', function() {
+    describe(connType + ': `server with an app', function() {
+      it('should use the fallback on unknown routes', function(done) {
+        var thisClient = function(srv, stub) {
+          stub.post(path, msg, {}, function(response) {
+            var theStatus;
+            var theError;
+            response.on('data', _.noop);
+            response.on('status', function(status) {
+              theStatus = status;
+            });
+            response.on('error', function(err) {
+              theError = err;
+            });
+            response.on('end', function() {
+              expect(theStatus).to.deep.equal({
+                'message': '',
+                'code': nurpc.rpcCode('UNKNOWN')
+              });
+              expect(theError).to.deep.equal({
+                'message': '',
+                'code': nurpc.rpcCode('UNKNOWN')
+              });
+              srv.close();
+              done();
+            });
+          });
+        };
+
+        var appOptions = _.clone(serverOptions);
+        appOptions.app = testApp;
+        var fallback = function fallback(request, response) {
+          // use a different status code than unknown
+          response.rpcCode = nurpc.rpcCode('UNKNOWN');
+          response.end('');
+        }
+        // here, null === no requestListener fallback
+        checkClientAndServer(thisClient, fallback, appOptions);
+      });
+      it('should use `nurpc.notFound` as the default fallback', function(done) {
+        var thisClient = function(srv, stub) {
+          stub.post(path, msg, {}, function(response) {
+            var theStatus;
+            var theError;
+            response.on('data', _.noop);
+            response.on('status', function(status) {
+              theStatus = status;
+            });
+            response.on('error', function(err) {
+              theError = err;
+            });
+            response.on('end', function() {
+              expect(theStatus).to.deep.equal({
+                'message': '',
+                'code': nurpc.rpcCode('NOT_FOUND')
+              });
+              expect(theError).to.deep.equal({
+                'message': '',
+                'code': nurpc.rpcCode('NOT_FOUND')
+              });
+              srv.close();
+              done();
+            });
+          });
+        };
+
+        var appOptions = _.clone(serverOptions);
+        appOptions.app = testApp;
+        // here, null === no requestListener fallback
+        checkClientAndServer(thisClient, null, appOptions);
+      });
+      it('should respond on registered handlers', function(done) {
+        var thisClient = function(srv, stub) {
+          stub.post('/test/do_echo', msg, {}, function(response) {
+            var theStatus;
+            var theError;
+            response.on('data', function(data) {
+              expect(data.toString()).to.eql(msg);
+            });
+            response.on('status', function(status) {
+              theStatus = status;
+            });
+            response.on('error', function(err) {
+              theError = err;
+            });
+            response.on('end', function() {
+              expect(theStatus).to.deep.equal({
+                'message': '',
+                'code': nurpc.rpcCode('OK')
+              });
+              expect(theError).to.be.undefined;
+              srv.close();
+              done();
+            });
+          });
+        };
+
+        var appOptions = _.clone(serverOptions);
+        appOptions.app = testApp;
+        checkClientAndServer(thisClient, _.noop, appOptions);
+      });
+      it('should use the specified encoder on the response', function(done) {
+        var thisClient = function(srv, stub) {
+          stub.post('/test/do_reverse', msg, {}, function(response) {
+            var want = reverser(msg);
+            response.on('data', function(data) {
+              expect(data).to.eql(want);
+            });
+            response.on('end', function() {
+              srv.close();
+              done();
+            });
+          });
+        };
+
+        var appOptions = _.clone(serverOptions);
+        appOptions.app = testApp;
+        checkClientAndServer(thisClient, _.noop, appOptions);
+      });
+      it('should use the specified decoder on the request', function(done) {
+        var thisClient = function(srv, stub) {
+          var sent = reverser(msg).toString();
+          stub.post('/test/do_irreverse', sent, {}, function(response) {
+            response.on('data', function(data) {
+              expect(data.toString()).to.eql(msg);
+            });
+            response.on('end', function() {
+              srv.close();
+              done();
+            });
+          });
+        };
+
+        var appOptions = _.clone(serverOptions);
+        appOptions.app = testApp;
+        checkClientAndServer(thisClient, _.noop, appOptions);
+      });
+    });
+    describe(connType + ': `nurpc.makeDispatcher`', function() {
       it('should respond with rpcCode 404 for empty table', function(done) {
         var thisClient = function(srv, stub) {
           stub.post(path, msg, {}, function(response) {
@@ -88,7 +252,7 @@ describe('RpcServer', function() {
           });
         };
 
-        checkClientAndServer(thisClient, server.makeDispatcher(), serverOptions);
+        checkClientAndServer(thisClient, nurpc.makeDispatcher(), serverOptions);
       });
       it('should respond with rpcCode 404 for unknown routes', function(done) {
         var thisClient = function(srv, stub) {
@@ -119,7 +283,7 @@ describe('RpcServer', function() {
 
         var table = _.clone(testTable);
         delete table['/x'];
-        var dispatcher = server.makeDispatcher(table);
+        var dispatcher = nurpc.makeDispatcher(table);
         checkClientAndServer(thisClient, dispatcher, serverOptions);
       });
       it('should respond for configured routes', function(done) {
@@ -146,11 +310,11 @@ describe('RpcServer', function() {
           });
         };
 
-        var dispatcher = server.makeDispatcher(testTable);
+        var dispatcher = nurpc.makeDispatcher(testTable);
         checkClientAndServer(thisClient, dispatcher, serverOptions);
       });
     })
-    describe(connType + ': `server.notFound`', function() {
+    describe(connType + ': `nurpc.notFound`', function() {
       it('should respond with rpcCode 404', function(done) {
         var thisClient = function(srv, stub) {
           stub.post(path, msg, {}, function(response) {
@@ -178,7 +342,7 @@ describe('RpcServer', function() {
           });
         };
 
-        checkClientAndServer(thisClient, server.notFound, serverOptions);
+        checkClientAndServer(thisClient, nurpc.notFound, serverOptions);
       });
     })
     describe(connType + ': simple request/response', function() {
