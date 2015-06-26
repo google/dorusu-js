715aafd3850725aa05d8f4aa49d1439b852f61f6
Tim Emiola <temiola@google.com>
2015-06-26 10:54:12 +0200

    Completes the math_server implementation.
    TESTED: used a client in another language to access it successfully.

--- example/math_server.js
+++ example/math_server.js
@@ -7,29 +7,22 @@ var protobuf = require('../lib/protobuf');
 var nurpc = require('../lib/nurpc');
 var server = require('../lib/server');
 
-var logOutput = process.stderr;
-var logger = bunyan.createLogger({
-  name: 'math_server',
-  stream: logOutput,
-  serializers: require('http2').serializers
-});
-
 /**
- * Server function for division.
+ * Implements math server division.
  *
  * Supports the /Math/DivMany and /Math/Div handlers
  * (Div is just DivMany with only one stream element). For each
  * DivArgs parameter, responds with a DivReply with the results of the division
  *
- * @param {Object} call The object containing request and cancellation info
- * @param {function(Error, *)} cb Response callback
+ * @param {Object} request the request stream
+ * @param {Object} response the response stream
  */
 function mathDiv(request, response) {
   request.on('data', function(msg) {
-    console.log('received', msg);
     if (+msg.divisor === 0) {
       response.rpcMessage = 'cannot divide by zero';
-      response.rpcCode = nurpc.rpcCodes('INVALID_ARGUMENT');
+      response.rpcCode = nurpc.rpcCode('INVALID_ARGUMENT');
+      response.end();
     } else {
       response.write({
         quotient: msg.dividend / msg.divisor,
@@ -37,22 +30,86 @@ function mathDiv(request, response) {
       });
     }
   });
-  request.on('end', function(data) {
-    console.log('Ended with', data);
+  request.on('end', function() {
+    response.end();
+  });
+  request.on('error', function() {
+    response.end();
   });
 };
 
-var main = function main() {
-  var mathpb = protobuf.loadProto(path.join(__dirname, 'math.proto'));
-  var mathApp = new app.RpcApp(mathpb.math.Math.server);
-  mathApp.register('/math.Math/DivMany', mathDiv);
-  mathApp.register('/math.Math/Div', mathDiv);
-  mathApp.register('/math.Math/Fib', nurpc.notFound);
-  mathApp.register('/math.Math/Sum', nurpc.notFound);
+/**
+ * Implements math server summation.
+ *
+ * Supports the /Math/Sum handler. `request` is a stream `Num`s, the response is
+ * written with their sum once the stream ends.
+ *
+ * @param {Object} request the request stream
+ * @param {Object} response the response stream
+ */
+function mathSum(request, response) {
+  // Here, call is a standard readable Node object Stream
+  var sum = 0;
+  request.on('data', function(data) {
+    sum += (+data.num);
+  });
+  request.on('end', function() {
+    response.end({num: sum});
+  });
+}
 
+/**
+ * Implements math server fibonacci.
+ *
+ * Supports the /Math/Fib handler. `request` is a `Num`, the response is
+ * stream consisting of fibonnaci sequence up to the value in the request.
+ *
+ * @param {Object} request the request stream
+ * @param {Object} response the response stream
+ */
+function mathFib(request, response) {
+  var previous = 0, current = 1;
+  request.on('data', function(msg) {
+    for (var i = 0; i < msg.limit; i++) {
+      response.write({num: current});
+      var tmp = current;
+      current += previous;
+      previous = tmp;
+    }
+    response.end();
+  });
+}
+
+/**
+ * Builds the `app.RpcApp` that provides the math service implementation
+ *
+ * - Creates the app with the service defined in math.proto
+ * - Registers the handlers as handlers
+ *
+ * @returns {app.RpcApp} providing the math service implementation
+ */
+var buildApp = exports.buildApp = function buildApp() {
+  var mathpb = protobuf.loadProto(path.join(__dirname, 'math.proto'));
+  var a = new app.RpcApp(mathpb.math.Math.server);
+  a.register('/math.Math/DivMany', mathDiv);
+  a.register('/math.Math/Div', mathDiv);
+  a.register('/math.Math/Fib', mathFib);
+  a.register('/math.Math/Sum', mathSum);
+  return a;
+}
+
+/**
+ * Provides a command line entry point when this file is run as a script.
+ */
+var main = function main() {
+  var log = bunyan.createLogger({
+    name: 'math_server',
+    stream: process.stderr,
+    serializers: require('http2').serializers
+  });
   var s = server.raw.createServer({
-    log: logger,
-    app: mathApp
+    log: log,
+    app: buildApp()
   });
   s.listen(50051);
 };
